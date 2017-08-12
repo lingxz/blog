98aa6723d48487e74a2643bbac1a2eaba9831e00
lingxz <lingyihuu@gmail.com>
2017-08-12 21:44:13 +0100

    edit travis build config to push to remote server

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -x
-if [ $TRAVIS_BRANCH == 'source' ] ; then
-    # Initialize a new git repo in _site, and push it to our server.
-    cd _site
-    git init
-    git remote add deploy "git@$SERVER_IP:/home/git/blog.git"
-    git config user.name "Travis CI"
-    git config user.email "lingyihuu+travisCI@gmail.com"
-    
-    git add .
-    git commit -m "Deploy"
-    git push --force deploy master
-else
-    echo "Not deploying, since this branch isn't master."
-fi
+cd _site
+git init
+git remote add origin 'ssh://git@178.62.18.237/home/git/blog.git'
+git config user.name "Travis CI"
+git config user.email "lingyihuu+travisCI@gmail.com"
+git add .
+git commit -am "new version $(date)" --allow-empty
+git push origin --mirror
